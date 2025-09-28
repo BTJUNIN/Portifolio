93df496da0aa80e27377a75e7c7c5717ad06d386
Marcio Jose <junior227733@gmail.com>
2025-09-28 20:25:23 -0300

    Corrigido o link do TryHackMe

--- api.js
+++
@@ -1,5 +0,0 @@
-async function getProfileData() {
-    const url = "https://raw.githubusercontent.com/BTJUNIN/Portifolio/refs/heads/main/curriculo-M%C3%A1rcio/data/dados.json";
-    const response = await fetch(url);
-    return await response.json();
-}
